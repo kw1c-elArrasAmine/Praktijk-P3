bc503a572e8d0ea266ae10f24d9abb69aeba65bd
Metin-Uysal <metin.uysal@edu-kw1c.nl>
2025-03-11 12:38:03 +0100

    test

--- test.php
+++ test.php
@@ -1,4 +1,19 @@
 <?php
-
-echo "skibidi";
+/**
+ * User: Uysal. Metin, El Arras. Amin & Van Lith. Tom
+ * Date: 3/11/2025
+ * File: T3_REA_Oefening2.2.php
+ */
 ?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        <?php
+        echo 'Krupp';
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../styles/styles.css" rel="stylesheet">
+</head>
