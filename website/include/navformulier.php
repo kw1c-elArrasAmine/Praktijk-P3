978cde7575a8da2dcbd1474bd0dc78f40c55aaca
Metin-Uysal <metin.uysal@edu-kw1c.nl>
2025-03-13 14:51:02 +0100

    Oefening 4.2

--- website/include/navformulier.php
+++ website/include/navformulier.php
@@ -1,20 +1,4 @@
-<?php
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<header>
-<ul>
-    <li><a href="#home">Home</a></li>
-    <li><a href="#news">News</a></li>
-    <li><a href="#contact">Contact</a></li>
-    <li style="float:right"><a class="active" href="#about">About</a></li>
-</ul><header>
-</body>
-</html>
+<div class="topnav">
+    <a href="index.php">Home</a>
+    <a href="formulier.php">Formulier</a>
+</div>
