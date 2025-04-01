84e8e693a612128a3fd2f6f75c0dd909e4ce9ec5
Metin-Uysal <metin.uysal@edu-kw1c.nl>
2025-04-01 23:21:30 +0200

    test

--- website/modellen.php
+++ website/modellen.php
@@ -28,8 +28,13 @@ include "include/navformulier.php";
 <div>
     <h2>PANZER II</h2>
     <img class="alfred" src="images/pzII.png" alt="Alfred Krupp">
-    <p>Panzerkampfwagen ausf II(afgekort panzer II) is een licht tank dat een armament heeft van 7.9mm machine wapen en 20mm auto kanon. Het tank is redelijk snel vergeleken met andere tank modellen met het snelheid van 44km/u maar een beter mobiliteit eist minder gewicht waardoor het tank model alleen 20mm frontaal pantser en 14mm zijden en achterkant pantser heeft. Het tank is meer geschikt voor scouting sinds vanwege zijn snelheid andere tanken kan opsporen en snel vluchten voordat het betrapt wordt. Het tank model is erg goedkoop vergeleken met andere modellen in de lijst, </p></div>
-
-
-
+    <p>Panzerkampfwagen ausf II(afgekort panzer II) is een licht tank dat een armament heeft van 7.9mm machine wapen en 20mm auto kanon. Het tank is redelijk snel vergeleken met andere tank modellen met het snelheid van 44km/u maar een beter mobiliteit eist minder gewicht waardoor het tank model alleen 20mm frontaal pantser en 14mm zijden en achterkant pantser heeft. Het tank is meer geschikt voor scouting sinds vanwege zijn snelheid andere tanken kan opsporen en snel vluchten voordat het betrapt wordt. Het tank model is erg goedkoop vergeleken met andere modellen in de lijst, met het prijs van 52.640 Reichmarks(247.408 Euro's) is dit tank model het meest massa produceerbaar. </p></div>
 </div>
+<div class="line"></div>
+<div class="margin" id="head">
+    <h2>PANZER IV</h2>
+    <img class="alfred" src="images/pzIV.png" alt="Alfred Krupp">
+    <p>Panzerkampfwagen ausf IV(afgekort panzer IV) is een medium tank, eerst geproduceerd in 1937 tot het einde van het tweede wereldoorlog in 1945 met verschillende varianten(Ausf A, B, C, D, E, F, G, H, en J). Panzer IV is het meest massaal geproduceerde tank van het oorlog met 8553 modellen geproduceerd inclusief het Sturmgesutz(stug), Jagdpanzer, Nashorn, Hummel, Brumbar, Ostwind, Wirbelwind en andere varianten. Het tank bevat 7,5 cm primaire kanon met 100mm penetrate in 500 meter afstand. Het tank heeft 50 tot 80mm frontaal pantser met 30mm bij het zijde en 20mm bij het achterkant. Het tank heeft een maximum snelheid van 42km/u maar bij oneven terrein is het snelheid significant vermindert tot 12km/u. Het prijs van het tank was 103,462 Reichmarks ofvtfr 486,271 in moderne euro's. Het tank is gebruikt in het Noord-Africa campagne, western front en oost front.</p>
+</div>
+</div>
+</body>
